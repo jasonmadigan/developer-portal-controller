c378febccb32acdf07639bd1ec01d6c098564972
dd <4183971+didierofrivia@users.noreply.github.com>
2025-11-12 15:41:24 +0100

    [api] APIKey scaffolding
    Signed-off-by: dd <4183971+didierofrivia@users.noreply.github.com>

---
+++ api/v1alpha1/apikey_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// APIKeySpec defines the desired state of APIKey.
+type APIKeySpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of APIKey. Edit apikey_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// APIKeyStatus defines the observed state of APIKey.
+type APIKeyStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// APIKey is the Schema for the apikeys API.
+type APIKey struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   APIKeySpec   `json:"spec,omitempty"`
+	Status APIKeyStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// APIKeyList contains a list of APIKey.
+type APIKeyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []APIKey `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&APIKey{}, &APIKeyList{})
+}
